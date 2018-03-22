0ef19acb00433364a8a87e16b527fa2aeda31be4
testsibirtsv <tablicaucheta12@gmail.com>
2018-03-22 17:54:55 +0200

    Change count of groups in test_delete_address precondition

--- test/test_delete_address_entry.py
+++ test/test_delete_address_entry.py
@@ -10,7 +10,7 @@ def test_delete_address_book_entry(conf):
     Delete the address book record by index and compare the length
     of the record list before and after deleting the address book record.
     """
-    if conf.address_book.entries_count() <= 2:
+    if conf.address_book.entries_count() < 2:
         conf.address_book.create(AddressBook(first_name="firstname1",
                                              last_name="lastname",
                                              address_1="address1",
